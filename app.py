b541d9d30edfe076f9ac31806a38b7f851cb3ef5
Sławomir Świętoniowski <60968459+sswietoniowski@users.noreply.github.com>
2022-02-01 14:21:02 +0100

    Played with app and request contexts

--- app.py
+++ app.py
@@ -1,16 +1,24 @@
 from flask import Flask
-
+from flask import request
+from flask import current_app
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    user_agent = request.headers.get('User-Agent')
+    app_name = current_app.name
+        
+    return f"""
+<h1>Hello World!</h1>
+<p>Your browser is: {user_agent}.</p>
+<p>Your app is: {app_name}.</p>
+"""
 
 
 @app.route('/user/<name>')
-def user(name):
+def user(name):    
     return f'<h1>Hello, {name}!</h1>'
 
 
